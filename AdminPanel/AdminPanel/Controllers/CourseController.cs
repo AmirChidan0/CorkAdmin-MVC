6172eeefa83f0fbd53d0e6652b7a9ab31a3ae0ec
amir hossein <amirhossein00978@gmail.com>
2025-03-07 02:31:37 +0330

    ایجاد کنترلر های مرود نیاز

---
+++ AdminPanel/AdminPanel/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminPanel.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
