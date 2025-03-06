c6a9e15fea765f1aa482bd498840240c402ad30a
amir hossein <amirhossein00978@gmail.com>
2025-03-07 02:50:35 +0330

    ساختار دیتابیس کور و دومین و کلاس ها ایجاد شد

--- AdminPanel/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ AdminPanel/Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1364747bec0f4650f902420c63cc4e1ce309420e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e24754eb4f6a8af5426f08c656db0eae7c4d01c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
